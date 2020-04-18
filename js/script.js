e35ff93519a5031d3c2bdf159be1d76d9abb0e2f
Clebson Pettenon <clebsonpettenon@gmail.com>
2020-04-18 15:10:06 -0300

    Finalizando o site

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 //Script rolagem suave
-$(".scroll").click(function (event) {
+$(".scroll").click(function(event) {
     event.preventDefault();
-    $('html,body').animate({scrollTop: $(this.hash).offset().top - 70}, 1000);
+    $('html,body').animate({ scrollTop: $(this.hash).offset().top - 70 }, 1000);
 });
